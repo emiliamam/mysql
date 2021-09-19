9f12737404f8bea115fba46c4544e929360e2fee
emiliamam <87708280+emiliamam@users.noreply.github.com>
2021-09-19 15:05:36 +0300

    config2.py

---
+++ config2.py
@@ -0,0 +1,24 @@
+import pymysql
+
+mydb = pymysql.connect(
+              host="localhost",
+              user="root",
+              passwd="",
+
+            )
+
+mycursor = mydb.cursor()
+mycursor.execute("CREATE DATABASE IF NOT EXISTS school_ITSH;")
+mydb = pymysql.connect(
+              host="localhost",
+              user="root",
+              passwd="",
+              database="school_ITSH"
+            )
+mycursor = mydb.cursor()
+
+# введите имя брокера/ip
+broker = ""
+
+# введите название темы, к которой нужно подписаться
+topic = ""
